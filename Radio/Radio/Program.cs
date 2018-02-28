f0d8617acf3c9fe5056d16669576be85831274c1
h3n7vi <kilbka02@gmail.com>
2018-02-28 12:00:18 +0700

    fifth commit

---
+++ Radio/Radio/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car viper = new Car();
+            viper.TurnOnRadio(false);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Radio/Radio/Radio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radio
+{
+    class Radio
+    {
+        public void Power(bool turnOn)
+        {
+            Console.WriteLine("Radio on: {0}", turnOn);
+
+        }
+    }
+    class Car
+    {
+        private Radio myRadio = new Radio();
+        public void TurnOnRadio(bool onOff)
+        {
+            myRadio.Power(onOff);
+        }
+    }
+
+}
+
